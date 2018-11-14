81a805e1cc0cd0576bcfb4621611f898f60b1f54
System Administrator <root@192-168-1-6.tpgi.com.au>
2018-11-14 22:03:18 +1100

    setup fetching data

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import {addExpense, removeExpense, editExpense} from "./actions/expenses";
+import {startSetExpenses} from "./actions/expenses";
 import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import "./styles/styles.scss";
@@ -19,5 +19,9 @@ const jsx = (
     </Provider>
 );
 
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-ReactDOM.render(jsx, document.getElementById("app"));
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById("app"));
+});
+
