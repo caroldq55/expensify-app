d30092ffb79eb571af67db62b834cfd365f67083
System Administrator <root@192-168-1-6.tpgi.com.au>
2018-11-17 21:29:01 +1100

    update

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -13,7 +13,7 @@ export default class ExpenseForm extends React.Component {
         this.state = {
             description: props.expense ? props.expense.description : "",
             note: props.expense ? props.expense.note : "",
-            amount: props.expense ? (props.expense.amount/100).toString() : "",
+            amount: props.expense ? (props.expense.amount).toString() : "",
             createAt: props.expense ? moment(props.expense.createAt) : moment(),
             calenderFocus: false,
             error: ""
